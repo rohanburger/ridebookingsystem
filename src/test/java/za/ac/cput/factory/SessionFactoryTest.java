33aaa87dd4cacabc345c2bf177a55c90788b7b34
rjanz <rjanzenburger@gmail.com>
2025-03-24 15:01:14 +0200

    Create test package for session , update pom.xml

--- src/test/java/za/ac/cput/factory/SessionFactoryTest.java
+++ src/test/java/za/ac/cput/factory/SessionFactoryTest.java
@@ -6,21 +6,5 @@ import za.ac.cput.domain.*;
 
 class SessionFactoryTest {
 
-    private Passenger passenger;
-    private Driver driver;
-    private Location location;
-
-    @BeforeEach
-    void setUp() {
-        passenger = new Passenger();  // You need to define how Passenger is created
-        driver = new Driver();        // Define the Driver class properly
-        location = new Location();    // Define the Location class properly
-    }
-
-    @Test
-    void testCreateSession_Success() {
-
-    }
-
 
 }
